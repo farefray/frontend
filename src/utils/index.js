fa310bc17f316c66b2f4c3597c862f6f2f2a8fb9
Unknown <maksym.fedan@gmail.com>
2017-10-11 07:54:34 +0300

    bumped packages versions

--- src/utils/index.js
+++ src/utils/index.js
@@ -81,7 +81,7 @@
  export function getByteLen(val) {
    let len = 0
    for (let i = 0; i < val.length; i++) {
-     if (val[i].match(/[^\x00-\xff]/ig) != null) {
+     if (val[i].match(/[^\x00-\xff]/ig) != null) { // eslint-disable-line no-control-regex
        len += 1
      } else { len += 0.5 }
    }
