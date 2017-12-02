56359acb96e863058826cb0ed142411d6d9b3aa1
Unknown <maksym.fedan@gmail.com>
2017-12-03 00:35:06 +0200

    proper chart displaying on personal dashboard

---
+++ src/views/stats/charts/statsChart.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="echarts">
+    <IEcharts :option="opts" :loading="loading" @ready="onReady" @click="onClick"></IEcharts>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import IEcharts from 'vue-echarts-v3/src/full.vue'
+  import settings from './statsChart.settings'
+  const bows = require('bows')
+  const log = bows('LineChart')
+
+  function parseDate(timestamp) { // TODO avoid this *1000 hack
+    let date = new Date(timestamp)
+    console.log(date);
+    return [('0' + date.getDate()).slice(-2), ('0' + (date.getMonth() + 1)).slice(-2), date.getFullYear()].join('/')
+  }
+
+  export default {
+    name: 'LineChart',
+    components: {
+      IEcharts
+    },
+    props: ['chartdata'],
+    computed: {
+      balance() {
+        return this.chartdata && this.chartdata.balance !== undefined ? this.chartdata.balance : 0
+      }
+    },
+    data: () => ({
+      opts: {},
+      loading: true
+    }),
+    updated() {
+      console.log('updated')
+      console.log(this.chartdata)
+    },
+    watch: {
+      chartdata(val) {
+        console.log(val);
+      }
+    },
+    mounted() {
+      this.loading = true;
+
+      log('mounted');
+      log(this.chartdata);
+      if (this.chartdata !== null) {
+        let labels = []
+        let data = []
+        let balance = 0
+
+        for (let i = 0; i <= this.chartdata.length - 1; i++) {
+          let prediction = this.chartdata.reverse()[i]
+          console.log(parseDate(prediction.date))
+          labels.push(parseDate(prediction.date))
+
+          if (prediction.status === 'WON' || prediction.status[0] === 'WON' /* TODO make it string*/) {
+            balance += prediction.stake * prediction.final_odds - prediction.stake
+          } else {
+            balance -= prediction.stake
+          }
+
+          data.push({ value: balance, bet: prediction })
+        }
+
+        settings.series[0].data = data
+        settings.xAxis[0].data = labels
+        log(settings)
+        this.opts = settings
+        this.loading = false
+      }
+    },
+    methods: {
+      onReady(instance) {
+        log(instance)
+      },
+      onClick(event, instance, echarts) {
+        log(arguments)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .echarts {
+    width: 1200px;
+    height: 450px;
+    margin: auto;
+  }
+</style>
--- src/views/stats/dashboard.vue
+++ src/views/stats/dashboard.vue
@@ -1,67 +1,48 @@
 <template>
   <div class="components-container" style='height:100vh'>
-    <button @click="refreshChart()">Refresh</button>
-    <div class='chart-container'>
-      <stats-chart height='100%' width='100%' :chartdata="dataPoints"></stats-chart>
+    <button @click="rerender()">Refresh</button>
+    <div class='chart-container' v-if="show">
+      <stats-chart height='100%' width='100%' :chartdata="predictions"></stats-chart>
     </div>
   </div>
 </template>
 
 <script>
-  import statsChart from '@/components/Charts/statsChart'
+  import statsChart from './charts/statsChart'
   import { getPredictions } from '@/api/apipredictions'
-
-  function parseDate(timestamp) {
-    let date = new Date(timestamp * 1000)
-    return [('0' + date.getDate()).slice(-2), ('0' + (date.getMonth() + 1)).slice(-2), date.getFullYear()].join('/')
-  }
 
   export default {
     components: { statsChart },
     data() {
       return {
         predictions: null,
-        dataPoints: null
+        show: false
       }
     },
     mounted() {
       console.log('stats mounted')
       this.$nextTick(() => {
+        this.fillData();
+      })
+    },
+    methods: {
+      rerender() {
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+          console.log('re-render start')
+          this.$nextTick(() => {
+            console.log('re-render end')
+          })
+        })
+      },
+      fillData() {
         getPredictions()
           .then(response => {
             this.predictions = response.data
             console.log(this.predictions)
-            this.fillData()
+            this.rerender();
           });
-      })
-    },
-    methods: {
-      refreshChart() {
-        this.fillData()
-      },
-      fillData() {
-        let labels = []
-        let data = []
-        let balance = 0
-
-        for (let i = 0; i <= this.predictions.length - 1; i++) {
-          let prediction = this.predictions.reverse()[i]
-          labels.push(parseDate(prediction.date))
-
-          if (prediction.status === 'WON') {
-            balance += prediction.stake * prediction.final_odds - prediction.stake
-          } else {
-            balance -= prediction.stake
-          }
-
-          data.push({ value: balance, bet: prediction })
-        }
-
-        this.dataPoints = {
-          labels: labels,
-          data: data,
-          balance: balance
-        }
       }
     }
   }
