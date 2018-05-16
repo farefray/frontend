78efd40b98898fbac2100818c3a07de6f2678c30
Unknown <maksym.fedan@gmail.com>
2018-05-16 09:28:40 +0300

    preview.js for production

--- build/preview.js
+++ build/preview.js
@@ -1,7 +1,10 @@
 var server = require("pushstate-server");
 
 server.start({
-    port: 80,
-    directory: './dist',
-    file: '/index.html'
+  port: 80,
+  host: "172.31.28.43",
+  directory: "../dist",
+  file: "/index.html"
 });
+
+console.log("> Serving server...\n");
